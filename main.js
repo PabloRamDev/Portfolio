90b2ccdaf5425b29cd466b492120a72934504a27
PabloRamirezMartinez <pabloram.dev@gmail.com>
2023-02-19 19:33:52 -0300

    first production upload

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import animate from "./banner3d.js";
-import aboutmeTween from "./aboutmetween.js";
-import { projectsTween } from "./projects_tween.js";
+import { traducciones } from "./traductions.js";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const banner3dTitle = document.querySelector("#banner3d_title")
 const banner3dcanvas = document.querySelector("#bg")
@@ -27,21 +29,171 @@ duration: 0.5},
 
 animate()
 banner3dTween()
-aboutmeTween()
-projectsTween()
+ScrollTrigger.config({
+    autoRefreshEvents: "visibilitychange,DOMContentLoaded,load,resize" // notice "resize" isn't in the list
+  });
+
+const aboutMeText = document.querySelector("#about_me_text_wrapper")
+const aboutMeWrap = document.querySelector("#about_me_image_wrapper")
+const aboutMeImage = document.querySelector("#about_me_image")
+
+const aboutmeTween = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".about_me_trigger",
+        start: "top top",
+        end: "+=100%",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+       
+        }
+    
+})
+aboutmeTween.from(
+    aboutMeText,
+    {x: "-100%",
+},
+
+).from(
+    aboutMeWrap,{
+        alpha: 0,
+        y: "100%",
+    },
+).from(
+    aboutMeImage,{ 
+        translateY: "100%",
+    },
+    
+)
+
+const projects = gsap.utils.toArray(".project")
+const stops = []
+
+projects.forEach((project, index) => {
+    if (project.dataset.pin) stops.push(index);
+    console.log(project.dataset)
+})
 
 
 
-function scrollAbout(){
-    e.preventDefault()
-    console.log("hola")
-  
-    window.scrollTo(0, Position(document.querySelector(".about_me_trigger")));
+const projectsTween = gsap.timeline({
+    defaults: {ease: "none"},
+    scrollTrigger: {
+        trigger: ".my_projects_trigger",
+        start: 'center center',
+        end: '+=4000',
+        scrub: true,
+        pin: true,
+        anticipatePin: 1
+        
+       
+        
+        
+    }
+})
+// projects
+stops.forEach((stop, index) => {
+    const actual = gsap.utils.selector(projects[stop])
+    projectsTween.to(projects, {
+        xPercent: (-100 * stop),
+        duration: stop,
+        
+    }).from(
+actual(".project_title"),{
+    alpha: 0
+}
+    ).from(
+       actual(".project_description"),{
+        ease: "power1",
+        alpha: 0,
+        xPercent: 300 ,
+       } ,
+       "<"
+    ).from(
+        
+        actual(".project_image_wrapper"),{
+            alpha: 0,
+         xPercent:300,
+         
+        } ,
+        "<"
+     ).to(
+        actual(".project_image_wrapper"),{
+            
+         yPercent:-3,
+         
+        } ,
+        "<"
+     )
+    
+    if(index === stops.length -1){
+        projectsTween.to(projects, {
+            xPercent: -(100 * (projects.length -1)),
+            duration: projects.length - stop,
+            
+        })
+    }
+})
+
+
+
+const translate = (elem,obj,lang)=>{
+   
+    elem.forEach(el => {
+        
+        const trad = Object.keys(obj[lang])
+        trad.forEach(tr => {
+            if(el.id == tr){
+                el.innerHTML = obj[lang][tr]
+            }
+        })
+   
+})
+}
+
+const elements = document.querySelectorAll(["p","h2","h3","a"])
+
+const handleTranslate = (e)=>{
+    var lang = document.documentElement.lang
+    
+    if( lang == 'en'){
+        lang = 'es'
+        document.documentElement.lang = 'es'
+        e.target.innerText = '🇪🇸'
+        translate(elements,traducciones, lang)
+      
+       
+    }else{
+        lang = 'en'
+        document.documentElement.lang = 'en'
+        e.target.innerText = '🇬🇧'
+        translate(elements,traducciones, lang)
+        
+        
+    }
+
+
+    
+}
+let languageBtn = document.querySelector("#language");
+
+const  languageStart = ()=>{
+    var userLang = navigator.language || navigator.userLanguage; 
+    console.log(userLang)
+    if(userLang.includes('es')){
+        
+        translate(elements,traducciones,'es')
+        document.documentElement.lang = 'es'    
+        languageBtn.innerText = '🇪🇸'
     
 
+    }
 }
-const about_link = document.querySelector(".about_link")
-about_link.addEventListener("click",scrollAbout);
+
+
+
+window.addEventListener('DOMContentLoaded',languageStart)
+languageBtn.addEventListener('click', handleTranslate)
 
 
 
---
+++ traductions.js
@@ -0,0 +1,58 @@
+const traducciones = {
+    en: {
+        about_nav: "about me",
+        my_projects_nav: "my projects",
+        contact_nav: "contact me",
+        banner3d_subtitle : "Full Stack Developer",
+        about_me_title: "About Me",
+        about_me_p1: " I'm a freshly graduated <span class='fullstack'>Full Stack Developer</span> looking for a new job. I live in Chile and i have 3 years of experience working in IT related fields (support), but web development is what i'm really into. <br/> Express.js and Django (Django REST Framework) are the backend frameworks that i'm using to build fast and reliable apps.",
+        about_me_p2:"In relation to frontend development, i'm really comfortable working with React.js Next.js and CSS frameworks like bootstrap and tailwind.",
+        about_me_p3: "Now i'm currently learning Three.js and GSAP to make my sites pop with 3D graphics and cool animations.",
+        project_0_title: "My <span>projects</span>",
+        project1_p1: "Web APP made with Django Rest Framework and a frontend made with React.Js and Material-Ui (MUI v5) component library.",
+        project_p2: "The main use of APP reservas DAE is to make reservations of the Multi-Purpose Sports Field of Inacap La Serena.",
+        project2_h3: "<span>&#60;</span> CPA fixed Assets System <span>/&#62;</span>",
+        project2_p1: "Web APP made with Django Rest Framework and a frontend made with React.Js and Material-Ui (MUI v5) component library.",
+        project2_p2: "The main use of this App is to keep an active database of the fixed Assets of an institution, Inventory etc.",
+        project3_h3: "<span>&#60;</span> PABLORAM.DEV Portfolio <span>/&#62;</span>",
+        project3_p1:"Well, This is the portfolio you're currently browsing, Made with Vanilla Js, HTML5 and CSS3.",
+        project3_p2: "GSAP is the library used to make the scroll animations and a the little 3D Cube of my Stacks is made with THREE.Js",
+        contact_h2: "Contact <span>Me</span>",
+        contact_p: "If you liked my work, or you are hiring, feel free to contact me using any of the options below",
+        linkedin_p:"Connect on LinkedIn",
+        github_p: "Explore my Github",
+        email_p: "Send me an Email",
+
+
+    },
+    es: {
+        about_nav: "sobre mí",
+        my_projects_nav: "mis proyectos",
+        contact_nav: "contacto",
+        banner3d_subtitle : "Analista Programador",
+        about_me_title: "Sobre mí",
+        about_me_p1: " Soy egresado de la carrera <span class='fullstack'>Analista Programador</span> y cuento con 3 años de experiencia en cargos de soporte TI. y actualmente, me encuentro buscando un trabajo de desarrollador web, donde pueda desempeñarme en lo que me apasiona. Las herramientas que utilizo para el desarrollo backend son Express.Js y Django (Rest Framework). ",
+        about_me_p2:"Con respecto al desarrollo frontend, React.js, Next.js y Frameworks CSS como bootstrap y tailwind, son las herramientas con las que estoy más familiarizado.",
+        about_me_p3: "Actualmente me encuentro aprendiendo GSAP y THREE.Js para hacer que mis sitios destaquen con gráficos en 3D y animaciones",
+        project_0_title: "Mis <span>proyectos</span>",
+        project1_p1: "Aplicación Web hecha con Django Rest Framework y un frontend hecho con React.Js y la librería de componentes Material Ui (MUI v5).",
+        project1_p2: "App Reservas DAE se desarrolló con el fin de generar una herramienta para administrar las reservas y solicitudes de la multicancha de Inacap Sede La Serena.",
+        project2_h3: "<span>&#60;</span> Sistema de activos fijos CPA <span>/&#62;</span>",
+        project2_p1: "Aplicación Web hecha con Django Rest Framework y un frontend hecho con React.Js y la librería de componentes Material Ui (MUI v5).",
+        project2_p2: "El principal uso de esta aplicación consiste en llevar un registro en una base de datos de inventario de activos fíjos de una institución educativa y generar códigos de barra e informes para cada uno de ellos",
+        project3_h3: "<span>&#60;</span> Portafolio PABLORAM.DEV <span>/&#62;</span>",
+        project3_p1:"Este es mi sitio de portafolio, todo está construido con HTML5, css puro y javascript.",
+        project3_p2:"El pequeño cubo con los logotipos de los stacks teconológicos fue hecho con THREE.Js y las animaciones con ScrollTrigger fueron hechas con GSAP.",
+        contact_h2: "Contácta<span>me</span>",
+        contact_p: "Si te gustó mi trabajo, puedes usar cualquiera de las opciones de abajo para contactarme.",
+        linkedin_p:"Conectar en Linkedin",
+        github_p: "Sígueme en Github",
+        email_p: "Envíame un email",
+
+    }
+  
+}
+
+export {
+    traducciones
+}
